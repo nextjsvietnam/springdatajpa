d99267ecb67fac6a4d3f99af0ddbc00a8012f293
misostack <misostack.com@gmail.com>
2024-06-25 12:46:11 +0700

    Customize security config

--- src/main/java/net/re/springdatajpa/config/SecurityConfig.java
+++ src/main/java/net/re/springdatajpa/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,12 +15,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//            Customizer<CsrfConfigurer<HttpSecurity>> customizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//
+//                @Override
+//                public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+//                    httpSecurityCsrfConfigurer.disable();
+//                }
+//            };
+//            http.csrf(customizer);
+//            Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> cusHttp = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+//                @Override
+//                public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+//                    authorizationManagerRequestMatcherRegistry.anyRequest();
+//                }
+//            };
+//            http.authorizeHttpRequests(cusHttp);
 
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeRequests(request -> request.anyRequest().authenticated());
-        // http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(customizer -> customizer.disable())
+                .authorizeRequests(request -> request.anyRequest().authenticated())
+                // .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         return http.build();
     }
 }
